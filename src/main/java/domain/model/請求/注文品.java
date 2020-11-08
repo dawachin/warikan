a7d7a896d609b91e31afa82d10cfeec891104d81
hiroaki ohwada <capriccioso.and.grandioso@gmail.com>
2020-11-08 20:31:11 +0900

    [update] 間違った注文品はインスタンスとして生成できない

--- src/main/java/domain/model/請求/注文品.java
+++ src/main/java/domain/model/請求/注文品.java
@@ -12,5 +12,19 @@ public class 注文品 {
 
     private boolean 持ち帰り;
 
+    public 注文品(String 注文品名, Integer 単価, Integer 個数, boolean 持ち帰り){
+
+        if (単価 < 0) throw new RuntimeException("単価がマイナスになっています");
+        if (個数 < 0) throw new RuntimeException("個数がマイナスになっています");
+        if (持ち帰り && 個数 > 200) throw new RuntimeException("持ち帰りのときは201個以上は注文できません");
+
+        this.注文品名 = 注文品名;
+        this.単価 = new BigDecimal(単価);
+        this.個数 = 個数;
+        this.持ち帰り = 持ち帰り;
+    }
+
+
+
 
 }
---
+++ src/test/java/domain/model/請求/注文品Test.java
@@ -0,0 +1,40 @@
+package domain.model.請求;
+
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertWithMessage;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class 注文品Test {
+
+    @Test
+    public void そもそもマイナスの単価では注文品とは見なせないよね() {
+
+        // 単価や個数にマイナスが入っているとインスタンス自体が生成できない
+        // --> ”注文品”というモデルとしてあり得ない
+        Throwable 単価がマイナスでエラー = assertThrows(RuntimeException.class, ()->{
+            new 注文品("鉄板鶏餃子", -520, 2, false);
+        });
+        Throwable 個数がマイナスでエラー = assertThrows(RuntimeException.class, ()->{
+            new 注文品("鉄板鶏餃子", 520, -2, false);
+        });
+
+        assertWithMessage("エラーがスローされている事")
+                .that(単価がマイナスでエラー.getMessage()).isEqualTo("単価がマイナスになっています");
+        assertWithMessage("エラーがスローされている事")
+                .that(個数がマイナスでエラー.getMessage()).isEqualTo("個数がマイナスになっています");
+    }
+
+    @Test
+    public void 持ち帰りのときは個数の上限がある() {
+
+        // 持ち帰りのとき、個数を201以上になっているとインスタンス自体が生成できない
+        // --> ”注文品”というモデルとしてあり得ない
+        Throwable 持ち帰りなのに個数が500 = assertThrows(RuntimeException.class, ()->{
+            new 注文品("鉄板鶏餃子", 520, 500, true);
+        });
+
+        assertWithMessage("エラーがスローされている事")
+                .that(持ち帰りなのに個数が500.getMessage()).isEqualTo("持ち帰りのときは201個以上は注文できません");
+    }
+}
