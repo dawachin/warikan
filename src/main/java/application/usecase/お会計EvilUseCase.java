37307dcc32fecf05f1bf72dfa178931b94da9680
dawachin <capriccioso.and.grandioso@gmail.com>
2020-11-10 17:36:56 +0900

    [update] 指摘箇所反映

--- src/main/java/application/usecase/お会計EvilUseCase.java
+++ src/main/java/application/usecase/お会計EvilUseCase.java
@@ -10,7 +10,6 @@ public class お会計EvilUseCase {
 
     public long 合計金額を計算する(List<注文品Evil> 注文一覧){
 
-
         // 合計金額を0で初期化
         BigDecimal 合計金額 = BigDecimal.ZERO;
 
@@ -26,20 +25,18 @@ public class お会計EvilUseCase {
             if (注文品.get単価().compareTo(new BigDecimal(100000)) >= 0) throw new RuntimeException("対応できる単価を超えています");
 
             // 持ち帰りか否かで適用する税率が変わる
+            BigDecimal 小計;
             if (注文品.is持ち帰り()){
                 // 持ち帰りの時は軽減税率を適用
-                BigDecimal 小計 = 注文品.get単価().multiply(new BigDecimal(注文品.get個数())).multiply(軽減税率);
-                合計金額 = 合計金額.add(小計);
+                小計 = 注文品.get単価().multiply(new BigDecimal(注文品.get個数())).multiply(軽減税率);
             } else {
                 // 持ち帰りじゃない時は標準税率を適用
-                BigDecimal 小計 = 注文品.get単価().multiply(new BigDecimal(注文品.get個数())).multiply(標準税率);
-                合計金額 = 合計金額.add(小計);
+                小計 = 注文品.get単価().multiply(new BigDecimal(注文品.get個数())).multiply(標準税率);
             }
+            合計金額 = 合計金額.add(小計);
         }
 
         BigDecimal 丸めた金額 = 合計金額.setScale(0, BigDecimal.ROUND_HALF_UP);
         return 丸めた金額.longValue();
     }
-
-
 }
