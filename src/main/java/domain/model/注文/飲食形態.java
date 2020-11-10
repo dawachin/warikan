4c75572f4cf518081a00ff9656550e54852a6a0b
dawachin <capriccioso.and.grandioso@gmail.com>
2020-11-10 17:37:36 +0900

    [update] 税区分を飲食形態に持たせた

--- src/main/java/domain/model/注文/飲食形態.java
+++ src/main/java/domain/model/注文/飲食形態.java
@@ -1,6 +1,18 @@
 package domain.model.注文;
 
+import domain.model.請求.消費税率区分;
+
 public enum 飲食形態 {
-    店内,
-    持ち帰り
+    店内(domain.model.請求.消費税率区分.標準税率),
+    持ち帰り(domain.model.請求.消費税率区分.軽減税率);
+
+    private 消費税率区分 消費税率区分;
+
+    public 消費税率区分 税区分(){
+        return this.消費税率区分;
+    }
+
+    飲食形態(消費税率区分 消費税率区分){
+        this.消費税率区分 = 消費税率区分;
+    }
 }
