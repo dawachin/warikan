aeaeb4901967c8504610d8c10c0ffb2af7625c41
hiroaki ohwada <capriccioso.and.grandioso@gmail.com>
2020-11-16 00:02:40 +0900

    [update] 最大注文単価を定義

--- src/main/java/domain/model/注文/注文単価.java
+++ src/main/java/domain/model/注文/注文単価.java
@@ -6,6 +6,7 @@ import domain.type.金額;
 public class 注文単価 {
 
     private 金額 単価;
+    private final 金額 最大注文単価 = 金額.整数より(100000);
 
     public 注文単価(Integer 単価){
         this(金額.整数より(単価));
@@ -13,7 +14,7 @@ public class 注文単価 {
 
     public 注文単価(金額 単価){
         if (単価.次の金額より小さいか(金額.ゼロ())) throw new RuntimeException("単価がマイナスになっています");
-        if (単価.次の金額より大きいか(金額.整数より(100000))) throw new RuntimeException("対応できる単価を超えています");
+        if (単価.次の金額より大きいか(最大注文単価)) throw new RuntimeException("対応できる単価を超えています");
         this.単価 = 単価;
     }
 
