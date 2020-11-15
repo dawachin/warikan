fda8e4a24ea44bb3a4a9668e3483de04270ef7cc
hiroaki ohwada <capriccioso.and.grandioso@gmail.com>
2020-11-15 15:22:37 +0900

    [update] 不要になった飲食形態区分を撤去

--- src/main/java/domain/model/請求/消費税率区分.java
+++ src/main/java/domain/model/請求/消費税率区分.java
@@ -1,30 +1,20 @@
 package domain.model.請求;
 
-import domain.model.注文.飲食形態;
 import domain.type.百分率;
 
 public enum  消費税率区分 {
 
-    標準税率(消費税.整数より(10), domain.model.注文.飲食形態.店内),
-    軽減税率(消費税.整数より(10).軽減税率を適用(百分率.整数より(2)), domain.model.注文.飲食形態.持ち帰り);
+    標準税率(消費税.整数より(10)),
+    軽減税率(消費税.整数より(10).軽減税率を適用(百分率.整数より(2)));
 
     private 消費税 税率;
-    private 飲食形態 飲食形態;
 
-    消費税率区分(消費税 消費税, 飲食形態 飲食形態){
+    消費税率区分(消費税 消費税){
         this.税率 = 消費税;
-        this.飲食形態 = 飲食形態;
     }
 
     public 消費税 税率(){
         return this.税率;
-    }
-
-    public static 消費税率区分 飲食形態より決める(飲食形態 飲食形態){
-        for (消費税率区分 区分 : 消費税率区分.values()){
-            if(区分.飲食形態.equals(飲食形態))return 区分;
-        }
-        throw new RuntimeException("指定された飲食形態に該当する税率がありません");
     }
 
 }
