6d0eb1fefc2346dd8b57d593f58493b59ef1df9f
hiroaki ohwada <capriccioso.and.grandioso@gmail.com>
2020-11-15 21:40:37 +0900

    [update] 最大注文個数制約を追加

--- src/main/java/domain/model/注文/注文個数.java
+++ src/main/java/domain/model/注文/注文個数.java
@@ -6,8 +6,19 @@ public class 注文個数 {
 
     BigDecimal 個数;
 
+    BigDecimal 最大注文個数 = new BigDecimal(500);
+
+    /**
+     * コンストラクタ
+     *
+     * ■ ドメイン駆動設計でつくる
+     * このコンストラクタで"注文個数"という概念の制約を表現する
+     *
+     * @param 個数
+     */
     public 注文個数(Integer 個数){
         if ( 個数 < 0) throw new RuntimeException("個数がマイナスになっています");
+        if (new BigDecimal(個数).compareTo(最大注文個数) > 0) throw new RuntimeException("扱える最大注文個数を超えています");
         this.個数 = new BigDecimal(個数);
     }
 
@@ -18,6 +29,5 @@ public class 注文個数 {
     public BigDecimal 値() {
         return this.個数;
     }
-
 
 }
