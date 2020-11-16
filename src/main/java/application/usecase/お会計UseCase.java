5a4aad1a346ecf45c7d3b11b01e0aabd273bee8d
hiroaki ohwada <capriccioso.and.grandioso@gmail.com>
2020-11-16 23:47:18 +0900

    [update] テストコードも注文一覧に対応させapplicationクラスの無駄を全て削除

--- src/main/java/application/usecase/お会計UseCase.java
+++ src/main/java/application/usecase/お会計UseCase.java
@@ -1,18 +1,11 @@
 package application.usecase;
 
 import domain.model.注文.注文一覧;
-import domain.model.注文.注文品;
 import domain.type.金額;
-
-import java.util.List;
 
 public class お会計UseCase {
 
-    public 金額 合計金額を計算する(List<注文品> 注文一覧){
-
-        注文一覧 注文された一覧 = new 注文一覧(注文一覧);
-
-        金額 丸めた金額 = 注文された一覧.税込総合計金額を出す().四捨五入();
-        return 丸めた金額;
+    public 金額 合計金額を計算する(注文一覧 注文された一覧){
+        return 注文された一覧.税込総合計金額を出す().四捨五入();
     }
 }
